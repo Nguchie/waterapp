b05eb9d8e990d1b0d6332fa99a68b731d872dc03
Nguchie <110086339+nwattt@users.noreply.github.com>
2022-10-07 21:58:52 +0300

    Add files via upload

---
+++ water.py
@@ -0,0 +1,27 @@
+import time
+
+def water_app():
+    drink=0
+    while drink<6:
+        print("HEY")
+        print("TIME TO DRINK WATER")
+
+        drink+=1
+        time.sleep(2)
+
+water_app()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
